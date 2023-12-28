219fa4d09b3798ea4bac9c2465ee695251a67716
Gediminas Norkus <gediminasno91@gmail.com>
2023-12-28 20:40:19 +0200

    PD2-1

---
+++ src/lt/techin/HeavyMonday.java
@@ -0,0 +1,22 @@
+package lt.techin;
+
+import java.util.Scanner;
+
+public class HeavyMonday {
+    public static  String color ="Sveiki";
+
+
+    public static void main(String[] args) {
+        // TODO
+//        Scanner
+        int MyInt = 0;
+        Car car = new Car();
+        String naujas = "labukas";
+        System.out.println(MyInt);
+        System.out.println(color);
+        System.out.println(naujas);
+        System.out.println(car);
+
+
+    }
+}
